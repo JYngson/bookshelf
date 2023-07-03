ebd8bbd8947b56a0b1a2435b54352ee373fe2d80
tjarevalo <jimmyjamesyngson@gmail.com>
2023-07-02 20:23:44 -0700

    login impemented w nextauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,30 @@
 import Navbar from "@/components/navbar/navbar"
 import GoogleAuthButton from "@/components/auth/GoogleAuthButton"
+import { getServerSession } from "next-auth";
+import { authConfig } from "@/lib/auth";
 
-export default function Home() {
-  return (
-    <div className='flex flex-col w-screen h-screen base'>
-      <Navbar/>
-      <div className='flex h-full w-full justify-center items-center'>
-        <div className='flex justify-center items-center p-10 w-fit h-1/4 bg-white/50 rounded-xl'>
-          <GoogleAuthButton />
+export default async function Home() {
+  const session = await getServerSession(authConfig);
+  console.log(session);
+  if(session){
+    return(
+      <div className='flex flex-col w-screen h-screen base'>
+        <Navbar/>
+        <div className='flex h-full w-full justify-center items-center'>
+          <h1>Logged In.</h1>
         </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return(
+      <div className='flex flex-col w-screen h-screen base'>
+        <Navbar/>
+        <div className='flex h-full w-full justify-center items-center'>
+          <div className='flex justify-center items-center p-10 w-fit h-1/4 bg-white/50 rounded-xl'>
+            <GoogleAuthButton />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
